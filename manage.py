963552a4e42b41a5ed4bcab83db934830ada9224
dariokl <darioklopic@gmail.com>
2020-07-29 13:45:20 +0200

    fix

--- manage.py
+++ manage.py
@@ -5,8 +5,9 @@ from flask_migrate import MigrateCommand, Manager
 from dotenv import load_dotenv
 load_dotenv()
 
-manager = Manager(create_app('production'))
+manager = Manager(create_app(os.environ.get('FLASK_CONFIG')))
 manager.add_command('db', MigrateCommand)
 
+print(os.environ.get("FLASK_CONFIG"))
 if __name__ == '__main__':
     manager.run()
