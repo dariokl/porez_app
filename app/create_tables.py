6f4c3723b669e6946e4b3a82a100f052a701afc9
dariokl <darioklopic@gmail.com>
2020-07-29 13:23:49 +0200

    cpanel manage2

--- app/create_tables.py
+++
@@ -1,4 +0,0 @@
-from app import create_app, db
-
-with app.app_context():
-    db.create_all()
--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@ import os
 from app import create_app
 from flask_migrate import MigrateCommand, Manager
 
+from dotenv import load_dotenv
+load_dotenv()
+
 manager = Manager(create_app(os.environ.get('FLASK_CONFIG')))
 manager.add_command('db', MigrateCommand)
 
