5de7eee6ef7502bf998dd028974880247497e39f
dariokl <darioklopic@gmail.com>
2020-07-30 15:40:17 +0200

    schedule

--- app/__init__.py
+++ app/__init__.py
@@ -53,7 +53,7 @@ def create_app(config_name):
     from .users.views import scheduled_cleaning
 
     scheduler = BackgroundScheduler(daemon=True)
-    scheduler.add_job(scheduled_cleaning, 'interval', minutes=5)
+    scheduler.add_job(scheduled_cleaning, 'interval', hours=3)
     scheduler.start()
 
     db.create_all()
