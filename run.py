6d0ea1be8b486b94cab668e994a509dc69729ea4
dariokl <darioklopic@gmail.com>
2020-09-13 21:26:50 +0200

    server deploy two

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ load_dotenv()
 from app import create_app, db
 from flask_migrate import Migrate
 
-app = create_app(os.environ.get('FLASK_CONFIG'))
+app = create_app(os.environ.get('FLASK_CONFIG') or 'production')
 Migrate(app, db)
